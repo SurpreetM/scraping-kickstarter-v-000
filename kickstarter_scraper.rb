e194eb0c2e8e996ffc675cd9aa0acf1d77a01534
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-08-30 17:28:18 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,7 +8,7 @@ def create_project_hash
 
   projects = {}
 
-  kickstarter.css("li.project.grid_4") each do |project|
+  kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
@@ -16,7 +16,10 @@ def create_project_hash
       :location => project.css("ul.project-meta span.location-name").text,
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
-    end 
+    end
+
+    projects
+
   end
 
   # projects: kickstarter.css("li.project.grid_4")
@@ -25,8 +28,3 @@ def create_project_hash
   # description: project.css("p.bbcard_blurb").text
   # location: project.css("ul.project-meta span.location-name").text
   # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-
-
-projects
-
-end
